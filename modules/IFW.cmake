39c8aa64ec0696f75db0271149d6953074f89144
zasboth <zsolt.asboth@balabit.com>
2024-04-22 17:16:17 +0200

    IFW first rev

--- modules/IFW.cmake
+++ modules/IFW.cmake
@@ -1,3 +1,19 @@
 ####IFW specific variables
+include(CPackIFW)
 
 set(CPACK_IFW_PRODUCT_URL "https://github.com/balabit/qlogsystem")
+
+set(CPACK_IFW_TARGET_DIRECTORY "@HOMEDIR@/.local/lib/qlogsystem")
+
+set(cpack_ifw_configure_file "libqlogsyste.a" ${CPACK_IFW_TARGET_DIRECTORY})
+
+set(CPACK_COMPONENTS_ALL qlogsystem-project)
+
+cpack_ifw_configure_component(qlogsystem-project
+    FORCED_INSTALLATION
+    REQUIRES_ADMIN_RIGHTS
+    DISPLAY_NAME "qlogsystem"
+    DESCRIPTION ${CPACK_PACKAGE_DESCRIPTION_SUMMARY}
+    VERSION ${CPACK_PACKAGE_VERSION}
+    LICENSES "License" "LICENSE"
+    DEFAULT "true")
