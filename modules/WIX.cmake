ea96c67b7016b8bbeea3681bb4ff380e6115c7db
Botond Petras <botond.petras@balabit.com>
2024-08-16 14:51:39 +0200

    Add files for wix installer

--- modules/WIX.cmake
+++ modules/WIX.cmake
@@ -1,10 +1,13 @@
-include(CPackWIX)
+set(CPACK_WIX_VERSION 4)
 
-wix_add_project(libqlogsystem 
-    qlogsystem.wxs
-    OUTPUT_NAME "qlogsystem"
-    )
+#For replacing upgrade use a specific GUID with the syntax of XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX:
+#set(CPACK_WIX_UPGRADE_GUID )
 
-set(CPACK_GENERATOR WIX)
-set(CPACK_WIX_UPGRADE_GUID 21045d6a-016c-11ef-8ff5-4f5ef4a711f6)
-set(CPACK_WIX_LICENSE_RTF "")
+set(CPACK_WIX_LICENSE_RTF ${CMAKE_SOURCE_DIR}/safeguard_sta.rtf)
+set (CPACK_WIX_PRODUCT_ICON ${CMAKE_SOURCE_DIR}/picture.png)
+
+#This image must be 493 by 58 pixels:
+set(CPACK_WIX_UI_BANNER ${CMAKE_SOURCE_DIR}/wixui_bmp_banner.bmp)
+
+#This image must be 493 by 312 pixels:
+set(CPACK_WIX_UI_DIALOG ${CMAKE_SOURCE_DIR}/wixui_bmp_dialog.bmp)
